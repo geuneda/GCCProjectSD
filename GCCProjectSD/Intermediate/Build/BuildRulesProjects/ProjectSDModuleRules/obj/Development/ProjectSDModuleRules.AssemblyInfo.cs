a085317791934470e65b79a9a28d3e2b9be7b00e
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-04 14:44:03 +0900

    feat: 원거리 적 방향별 애니메이션 조정
    - 방향에 따른 애니메이션이 제대로 적용되지 않던 문제 해결
    - ABP 그래프에서 잘못된 계산 수정
    - 원거리 적 속도 조절

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+308702a61e33c1b748179a36e29d0ce988b2bfef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa990d5d06ba2a621db8f2b4e18559f110a07bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
