d2de4707d51d153f6642eb2a41ab6c5b2f6737d5
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-07 16:46:03 +0900

    Attack Interval, Attack Rebound [2025-04-07]
    Attack Interval and Attack rebound are completed. And, Muzzle effect is generated proper location.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01548d176f4a0f12b9939c3e7fe6e72cce1b240b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee6864f63239062d337fe9c4d5e79942be2dede")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
