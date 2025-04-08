8f1e4ca0c3a83e95dd4e372114c3a7395b8278c4
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-09 01:49:36 +0900

    Bullet Attack Effect [2025-04-08]
    Bullet Attack Effect generate / This function comes from line tracing attack (move this function)

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676693cb0f7f2014585a59fa1fe000511f0e07d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6e74ccd91235d82607411d7650ff5f3aa4e149")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
