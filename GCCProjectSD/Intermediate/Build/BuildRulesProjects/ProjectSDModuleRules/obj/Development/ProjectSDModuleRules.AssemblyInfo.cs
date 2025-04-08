63792e454ebccef1b48d11ef953e7213b842ec49
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-08 16:54:54 +0900

    chore: 프로젝트 셋팅 변경
    - 충돌 관련 설정 수정

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9cf4c95c6f26715be360d7f8366bb93f071853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09f3ba3e2d698969f7039ba1d6fd1ccc0476382")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
