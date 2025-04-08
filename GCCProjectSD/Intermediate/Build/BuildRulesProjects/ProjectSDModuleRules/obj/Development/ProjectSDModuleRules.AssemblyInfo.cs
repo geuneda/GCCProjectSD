8e9cf4c95c6f26715be360d7f8366bb93f071853
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-08 15:43:18 +0900

    Projectile Bullet Test [2025-04-07 to 08]
    Projectile Bullet Test

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee6864f63239062d337fe9c4d5e79942be2dede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1081c7a12ba332cdc8e4ae33b8d54d5b496c06c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
