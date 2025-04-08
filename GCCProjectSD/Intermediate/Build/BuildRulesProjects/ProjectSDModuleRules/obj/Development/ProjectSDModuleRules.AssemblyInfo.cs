c09f3ba3e2d698969f7039ba1d6fd1ccc0476382
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-08 16:49:42 +0900

    feat: 보스의 각 파츠의 소켓 적용
    - 애니메이션 적용을 위해 각 파츠에 알맞은 소켓을 적용

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1081c7a12ba332cdc8e4ae33b8d54d5b496c06c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9cf4c95c6f26715be360d7f8366bb93f071853")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
