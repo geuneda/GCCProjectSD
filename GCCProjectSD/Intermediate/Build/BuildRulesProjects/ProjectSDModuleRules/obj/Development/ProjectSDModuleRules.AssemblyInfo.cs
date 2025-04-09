392629661f813eda30ca3dccbf18cdd0d90c3042
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-09 17:04:25 +0900

    feat: 보스 랜덤 스킬 발동 구현
    - BT에서 랜덤확률로 스킬을 사용하도록 함
    - 공격에 쿨다운을 적용

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9755f91ca350bfbd86281938c115b31b071ec77c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db2be0df08e8f3b45cfc7a1397428fa70422ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
