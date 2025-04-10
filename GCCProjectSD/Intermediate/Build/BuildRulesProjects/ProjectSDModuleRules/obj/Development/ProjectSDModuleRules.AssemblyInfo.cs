fee3fed5afd8e706b2743557fddcafcd300da6d0
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-10 18:56:37 +0900

    Grappling Hook [2025-04-09 to 10]
    Action, Character Move. Shoot Hook, and Cable endpoint follow shooted hook. Grappling Hook and Hit wall, launch character system (with projection). Cable cut when hook system end. Hook system end system complete partly (Degree between Vector, Hook Time, Distance, Reusing hook action - forbidden) / 2025-04-09 to 10, new project file open for hook system conception and porting origin project file. so, 2days task in 1 commit.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6e74ccd91235d82607411d7650ff5f3aa4e149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1e4ca0c3a83e95dd4e372114c3a7395b8278c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
