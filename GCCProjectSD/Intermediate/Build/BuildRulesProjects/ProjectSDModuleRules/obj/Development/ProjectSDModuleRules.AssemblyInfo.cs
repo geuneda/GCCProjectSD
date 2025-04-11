1d8b04a7592005d2c54961393d22ca85b5d609e7
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-11 16:02:02 +0900

    Grappling Hook input move and cancle [2025-04-11]
    Input move system while grappling hook. When Jump and Attack, Grappling hook is cancled.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1e4ca0c3a83e95dd4e372114c3a7395b8278c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee3fed5afd8e706b2743557fddcafcd300da6d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
