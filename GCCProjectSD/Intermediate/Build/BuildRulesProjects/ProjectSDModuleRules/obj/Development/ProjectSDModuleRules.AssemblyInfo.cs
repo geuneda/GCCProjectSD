f1769ec2c14b5d0eab75c9771e1c5716810ae4a8
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-14 02:33:58 +0900

    GameOver System [2025-04-14]
    Fade Out when player is in gameover. And generating restart button for new game.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603c3f4a8af5b12f2008a8ade839e2eb7b710eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975fde231f9a8778cac479f2b339685dee72dc18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
