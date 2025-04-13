975fde231f9a8778cac479f2b339685dee72dc18
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-14 00:40:55 +0900

    ZSkill Targeting and Attack Enemy [2025-04-13]
    ZSkill Targeting and Attack Enemy. And Laser niagara effect generation system is completed with targeting system.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305b73aea6dfd016f843c144be51f6c480bd8987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603c3f4a8af5b12f2008a8ade839e2eb7b710eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
