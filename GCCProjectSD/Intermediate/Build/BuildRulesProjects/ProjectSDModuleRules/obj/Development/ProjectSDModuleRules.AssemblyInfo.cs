603c3f4a8af5b12f2008a8ade839e2eb7b710eed
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-13 16:02:55 +0900

    ZSkill Using System [2025-04-13]
    When you use zskill, zskill using system operates. - condition, eletric force reduce, cooltime, relation with other action, low speed etc. Exception for targeting and attack enemy logic.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fccc54eaa67110cb7f5f01fc8ea5fe27faaa64f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305b73aea6dfd016f843c144be51f6c480bd8987")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
