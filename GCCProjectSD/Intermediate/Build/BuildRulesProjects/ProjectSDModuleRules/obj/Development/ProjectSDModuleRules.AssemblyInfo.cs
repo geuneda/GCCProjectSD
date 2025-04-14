b4157f16241602bc1296b088ca8adbf488e1da91
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-14 19:03:35 +0900

    BeamSound [2025-04-14]
    BeamSound - SkillStart, BeamStart, BeamTick, Beam End.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dadd88f5bcdee5f0e649cfeeca9bee9988794207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf4a626004ecc5ce9f8daa83045dcb15feb5b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
