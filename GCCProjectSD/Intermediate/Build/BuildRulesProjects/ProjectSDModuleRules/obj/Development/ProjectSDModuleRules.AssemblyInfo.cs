045aa6e65e2adcbe7c3913f43f88f0b30deeb117
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-21 12:57:46 +0900

    RandomDamage For UI [2025-04-21]
    RandomDamage For UI. When Bullet apply damage, base damage is made using random float and to int for pretty UI.

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d68e26c68d61991bf67bd08e10e45f74ee3818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1490ab6a920600e5266ffb98677830e330186fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
