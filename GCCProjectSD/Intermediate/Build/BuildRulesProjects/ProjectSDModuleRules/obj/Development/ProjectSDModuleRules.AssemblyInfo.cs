d5846287c5a995862eb5abdf70ce61d7330fbdc3
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-04-21 17:53:20 +0900

    Forbidden Reload & Reload cancle system [2025-04-21]
    Forbidden Reload & Reload cancle system -> about hooking and skill4

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1490ab6a920600e5266ffb98677830e330186fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f90802c96ffce23ff0975e899e9d476782e1516")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
