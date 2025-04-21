c3d68e26c68d61991bf67bd08e10e45f74ee3818
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-21 09:26:41 +0900

    fix: 보스 관련버그 수정
    - 버서커패턴에서 공격을 성공했음에도 무적이라 표기되는 문제 수정
    - 다른 파츠 파괴에도 주사기 파괴로 인식하는 문제 수정

--- GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
+++ GCCProjectSD/Intermediate/Build/BuildRulesProjects/ProjectSDModuleRules/obj/Development/ProjectSDModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e2b45707bfea31abf07254e83ef72fdd55c99d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cc4f982e609ae953119b3968e1251794e9ed40")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSDModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
